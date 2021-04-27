4d873cee5903619999fb533dcefffc2dc4fac8e9
mauiii39 <77367289+mauiii39@users.noreply.github.com>
2021-04-26 20:51:47 -0500

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,3 @@
+if(!file.exists("./data")){dir.create("./data")}
+fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileurl, destfile = "./data/data.zip")
